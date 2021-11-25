de980d2261e1c550afdc5416a555eb14e1b466af
Yin Da <yd219913@alibaba-inc.com>
2021-11-25 16:22:27 +0800

    Fix: resourcetracker do not garbage collect legacyRTs when previousRT equals currentRT

--- pkg/controller/core.oam.dev/v1alpha2/application/dispatch/dispatch.go
+++ pkg/controller/core.oam.dev/v1alpha2/application/dispatch/dispatch.go
@@ -114,7 +114,8 @@ func (a *AppManifestsDispatcher) Dispatch(ctx context.Context, manifests []*unst
 	if err := a.applyAndRecordManifests(ctx, manifests); err != nil {
 		return nil, err
 	}
-	if !a.skipGC && a.previousRT != nil && a.previousRT.Name != a.currentRTName {
+	// TODO Problems: legacyRTs are garbage collected only if previousRT should be garbage collected?
+	if !a.skipGC && ((a.previousRT != nil && a.previousRT.Name != a.currentRTName) || len(a.legacyRTs) > 0) {
 		if err := a.gcHandler.GarbageCollect(ctx, a.previousRT, a.currentRT, a.legacyRTs); err != nil {
 			return nil, errors.WithMessagef(err, "cannot do GC based on resource trackers %q and %q", a.previousRT.Name, a.currentRTName)
 		}
